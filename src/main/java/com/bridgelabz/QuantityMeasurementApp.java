4cba3e8c69536069f89b3678b0a359b689118e13
Wave-Shot <vsh.2509@gmail.com>
2026-02-25 11:57:23 +0530

    [Srihari]: Added UC4-Extended Unit Support (Yards and Centimeters) using scalable enum-based design

--- src/main/java/com/bridgelabz/QuantityMeasurementApp.java
+++ src/main/java/com/bridgelabz/QuantityMeasurementApp.java
@@ -4,14 +4,24 @@ public class QuantityMeasurementApp {
 
     public static void main(String[] args) {
 
-        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
-        QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCH);
+        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.YARDS);
+        QuantityLength q2 = new QuantityLength(3.0, LengthUnit.FEET);
 
-        System.out.println("1.0 feet and 12.0 inches Equal? " + q1.equals(q2));
+        System.out.println("1 Yard == 3 Feet ? " + q1.equals(q2));
 
-        QuantityLength q3 = new QuantityLength(1.0, LengthUnit.INCH);
-        QuantityLength q4 = new QuantityLength(1.0, LengthUnit.INCH);
+        QuantityLength q3 = new QuantityLength(1.0, LengthUnit.YARDS);
+        QuantityLength q4 = new QuantityLength(36.0, LengthUnit.INCH);
 
-        System.out.println("1.0 inch and 1.0 inch Equal? " + q3.equals(q4));
+        System.out.println("1 Yard == 36 Inches ? " + q3.equals(q4));
+
+        QuantityLength q5 = new QuantityLength(1.0, LengthUnit.CENTIMETERS);
+        QuantityLength q6 = new QuantityLength(0.393701, LengthUnit.INCH);
+
+        System.out.println("1 CM == 0.393701 Inch ? " + q5.equals(q6));
+
+        QuantityLength q7 = new QuantityLength(2.0, LengthUnit.YARDS);
+        QuantityLength q8 = new QuantityLength(72.0, LengthUnit.INCH);
+
+        System.out.println("2 Yards == 72 Inches ? " + q7.equals(q8));
     }
 }
